b428c2fb6aea442b07739208eb0800ee7b9094ac
pooriaPoorsarvi <pooriapoorsarvi@gmail.com>
2025-11-29 18:35:22 +0000

    temp working solution communicating on icount needs cleaning

--- net/pdes-communicator.c
+++ net/pdes-communicator.c
@@ -4,78 +4,193 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include "qemu/timer.h"
 
-#define MAX_MSG_SIZE 2048
-
-typedef struct {
-    uint32_t len;
-    uint8_t data[MAX_MSG_SIZE];
-} Message;
 
 typedef struct {
     volatile uint32_t write_idx;
     volatile uint32_t read_idx;
-    Message messages[16];
+    Message           messages[RING_SIZE];
 } ShmRing;
 
+
 struct PDESCommunicator {
-    int fd;
-    ShmRing *ring;
-    size_t size;
+    int       fd_send;
+    int       fd_recv;
+    ShmRing  *ring_send;
+    ShmRing  *ring_recv;
+    size_t    size;
 };
 
-PDESCommunicator *pdes_comm_create(const char *shm_name, size_t shm_size) {
-    PDESCommunicator *comm = g_new0(PDESCommunicator, 1);
-    
-    comm->fd = shm_open(shm_name, O_CREAT | O_RDWR, 0666);
-    if (comm->fd < 0) {
-        g_free(comm);
+
+
+/* Helper to create/open and mmap a ring */
+static int pdes_comm_init_ring(const char *shm_name,
+                               int *fd_out,
+                               ShmRing **ring_out,
+                               size_t shm_size)
+{
+    int fd;
+    bool is_creator;
+    ShmRing *ring;
+
+    fd = shm_open(shm_name, O_CREAT | O_EXCL | O_RDWR, 0666);
+    is_creator = (fd >= 0);
+
+    if (!is_creator) {
+        /* Already exists, just open it */
+        fd = shm_open(shm_name, O_RDWR, 0666);
+        if (fd < 0) {
+            return -1;
+        }
+    } else {
+        /* We created it, so size it */
+        if (ftruncate(fd, shm_size) < 0) {
+            shm_unlink(shm_name);
+            close(fd);
+            return -1;
+        }
+    }
+
+    ring = mmap(NULL, shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (ring == MAP_FAILED) {
+        if (is_creator) {
+            shm_unlink(shm_name);
+        }
+        close(fd);
+        return -1;
+    }
+
+    if (is_creator) {
+        /* Initialize the ring buffer */
+        memset(ring, 0, shm_size);
+    } 
+    // else {
+    //     /* Wait for initialization by creator */
+    //     while (ring->write_idx == 0 && ring->read_idx == 0) {
+    //         usleep(1000);
+    //     }
+    // }
+    // TODO see if you need to add the above back in
+
+    *fd_out   = fd;
+    *ring_out = ring;
+    return 0;
+}
+
+
+PDESCommunicator *pdes_comm_create(const char *shm_send_name,
+                                   const char *shm_recv_name)
+{
+    PDESCommunicator *comm;
+    size_t shm_size = sizeof(ShmRing);
+
+    comm = g_new0(PDESCommunicator, 1);
+    if (!comm) {
         return NULL;
     }
-    
-    if (ftruncate(comm->fd, shm_size) < 0) {
-        close(comm->fd);
-        g_free(comm);
-        return NULL;
-    }
-    
-    comm->ring = mmap(NULL, shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, comm->fd, 0);
+
     comm->size = shm_size;
-    
+
+    if (pdes_comm_init_ring(shm_send_name, &comm->fd_send,
+                            &comm->ring_send, shm_size) < 0) {
+        g_free(comm);
+        return NULL;
+    }
+
+    if (pdes_comm_init_ring(shm_recv_name, &comm->fd_recv,
+                            &comm->ring_recv, shm_size) < 0) {
+        munmap(comm->ring_send, shm_size);
+        close(comm->fd_send);
+        g_free(comm);
+        return NULL;
+    }
+
     return comm;
 }
 
-void pdes_comm_destroy(PDESCommunicator *comm) {
-    if (comm->ring) {
-        munmap(comm->ring, comm->size);
+
+Message create_message(const uint8_t *data, size_t len, uint8_t type, uint64_t ts_ns)
+{
+    Message msg;
+    msg.ts_ns = ts_ns;
+    msg.len   = (uint32_t)len;
+    msg.type  = type;
+    memcpy(msg.data, data, len);
+    return msg;
+}
+
+
+void pdes_comm_destroy(PDESCommunicator *comm)
+{
+    if (!comm) {
+        return;
     }
-    if (comm->fd >= 0) {
-        close(comm->fd);
+
+    if (comm->ring_send) {
+        munmap(comm->ring_send, comm->size);
+    }
+    if (comm->ring_recv) {
+        munmap(comm->ring_recv, comm->size);
+    }
+    if (comm->fd_send >= 0) {
+        close(comm->fd_send);
+    }
+    if (comm->fd_recv >= 0) {
+        close(comm->fd_recv);
     }
     g_free(comm);
 }
 
-int pdes_comm_send(PDESCommunicator *comm, const uint8_t *data, size_t len) {
-    if (len > MAX_MSG_SIZE) return -1;
-    
-    uint32_t next_write = (comm->ring->write_idx + 1) % 16;
-    if (next_write == comm->ring->read_idx) return -EAGAIN;
-    
-    Message *msg = &comm->ring->messages[comm->ring->write_idx];
-    msg->len = len;
-    memcpy(msg->data, data, len);
-    qatomic_set_mb(&comm->ring->write_idx, next_write);
-    
-    return len;
+
+int pdes_comm_send(PDESCommunicator *comm, Message *msg)
+{
+
+    ShmRing *ring;
+    uint32_t next_write;
+
+    if (!comm || !comm->ring_send || !msg) {
+        printf("PDES Comm invalid parameters in send\n");
+        return -EINVAL;
+    }
+
+    ring = comm->ring_send;
+    next_write = (ring->write_idx + 1) % RING_SIZE;
+    while (next_write == ring->read_idx) {
+        // TODO check if we can make this better
+        // printf("PDES Comm ring buffer full, cannot send message now.\n");
+        // usleep(1000);  /* Wait for space to become available */
+        // return -EAGAIN;
+    }
+
+    // TODO check for race conditions
+    /* Copy the message into the ring */
+    ring->messages[ring->write_idx] = *msg;
+
+    qatomic_set_mb(&ring->write_idx, next_write);
+
+    printf("=========================== PDES Engine: Sent message of length %u and of type %u =========================== \n", msg->len, msg->type);
+    return 0;
 }
 
-int pdes_comm_recv(PDESCommunicator *comm, uint8_t *buf, size_t buf_len) {
-    if (comm->ring->read_idx == comm->ring->write_idx) return 0;
-    
-    Message *msg = &comm->ring->messages[comm->ring->read_idx];
-    size_t len = msg->len < buf_len ? msg->len : buf_len;
-    memcpy(buf, msg->data, len);
-    qatomic_set_mb(&comm->ring->read_idx, (comm->ring->read_idx + 1) % 16);
-    
-    return len;
+
+int pdes_comm_recv(PDESCommunicator *comm, Message *msg){
+    ShmRing *ring;
+    if (!comm || !comm->ring_recv || !msg) {
+        return -EINVAL;
+    }
+
+    ring = comm->ring_recv;
+    if (ring->read_idx == ring->write_idx) {
+        return NO_MESSAGE;  /* No message available */
+    }
+
+    /* Copy the message from the ring */
+    *msg = ring->messages[ring->read_idx];
+
+    qatomic_set_mb(&ring->read_idx, (ring->read_idx + 1) % RING_SIZE);
+
+    printf("=========================== PDES Engine: Received message of length %u and of type %u =========================== \n", msg->len, msg->type);
+    return msg->len;
 }
--- net/pdes-netdev.c
+++ net/pdes-netdev.c
@@ -9,33 +9,27 @@
 typedef struct PDESNetState {
     NetClientState nc;
     PDESEngine *engine;
-    QEMUTimer *poll_timer;
 } PDESNetState;
 
 static void pdes_net_cleanup(NetClientState *nc) {
     PDESNetState *s = DO_UPCAST(PDESNetState, nc, nc);
-    if (s->poll_timer) {
-        timer_free(s->poll_timer);
-    }
     if (s->engine) {
         pdes_engine_destroy(s->engine);
     }
 }
 
 static ssize_t pdes_net_receive(NetClientState *nc, const uint8_t *buf, size_t size) {
+    printf("^^^^^^^^^^^^^^ PDES Netdev receive (request to send) called with packet of length %zu ^^^^^^^^^^^^^^ \n", size);
     PDESNetState *s = DO_UPCAST(PDESNetState, nc, nc);
-    return pdes_engine_send(s->engine, buf, size);
+    int ret = pdes_engine_send(s->engine, buf, size);
+    printf("^^^^^^^^^^^^^^ PDES Netdev sending packet of length %zu with return value %d^^^^^^^^^^^^^^ \n", size, ret);   
+    return (ret < 0) ? ret : size;
 }
 
 static void pdes_recv_callback(void *opaque, const uint8_t *data, size_t len) {
     NetClientState *nc = opaque;
+    printf("^^^^^^^^^^^^^^ PDES Netdev received packet of length %zu^^^^^^^^^^^^^^ \n", len);
     qemu_send_packet(nc, data, len);
-}
-
-static void pdes_poll_timer_cb(void *opaque) {
-    PDESNetState *s = opaque;
-    pdes_engine_poll(s->engine);
-    timer_mod(s->poll_timer, qemu_clock_get_ms(QEMU_CLOCK_VIRTUAL) + 1);
 }
 
 static NetClientInfo net_pdes_info = {
@@ -51,11 +45,15 @@ int net_init_pdes(const Netdev *netdev, const char *name, NetClientState *peer, 
     NetClientState *nc = qemu_new_net_client(&net_pdes_info, peer, "pdes", name);
     PDESNetState *s = DO_UPCAST(PDESNetState, nc, nc);
     
-    s->engine = pdes_engine_create(pdes_opts->shm_send, pdes_opts->shm_recv);
-    pdes_engine_set_recv_callback(s->engine, pdes_recv_callback, nc);
+    s->engine = pdes_engine_create(pdes_opts->shm_send, pdes_opts->shm_recv, pdes_opts->sync, pdes_opts->latencyns, pdes_recv_callback, nc);
+    // nc->link_down = false;
     
-    s->poll_timer = timer_new_ms(QEMU_CLOCK_VIRTUAL, pdes_poll_timer_cb, s);
-    timer_mod(s->poll_timer, qemu_clock_get_ms(QEMU_CLOCK_VIRTUAL) + 1);
-    
+    // // Notify peer (the emulated NIC) about link status
+    // if (nc->peer && nc->peer->info->link_status_changed) {
+    //     nc->peer->info->link_status_changed(nc->peer);
+    // }
+    printf("PDES Netdev initialized with shm_send=%s, shm_recv=%s, sync=%d, latencyns=%lu\n",
+           pdes_opts->shm_send, pdes_opts->shm_recv, pdes_opts->sync, pdes_opts->latencyns);
+
     return 0;
 }
